ed261f16d75cd6ce906eee3b72d11ae08537b3f6
Chris Maltais <chris.maltais@hotmail.ca>
2017-08-28 23:53:15 +0800

    Add 1 file via upload
    dictionary_to_json.py

---
+++ dictionary_to_json.py
@@ -0,0 +1,12 @@
+# Store the dictionary in a json file.
+
+d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+                {"firstName": "Anna", "lastName": "Smith"},
+                {"firstName": "Peter", "lastName": "Jones"}],
+"owners":[{"firstName": "Jack", "lastName": "Petter"},
+          {"firstName": "Jessy", "lastName": "Petter"}]}
+
+import json
+
+with open("dictionary", "w") as f:
+    json.dump(d, f, indent = 4, sort_keys = True)
